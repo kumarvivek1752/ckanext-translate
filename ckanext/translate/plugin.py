9a65cc65b83f43d6bcc52025fe8dce3788edde74
Vivek Kumar <Kumarvivek1752@gmail.com>
2023-08-03 10:56:47 +0530

    fixed translation

--- ckanext/translate/plugin.py
+++ ckanext/translate/plugin.py
@@ -1,6 +1,8 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+from ckan.lib.plugins import DefaultTranslation
+
 
 # ckanext_multilingual/ckanext_multilingual/plugin.py
 # from ckanext.scheming import plugin as scheming_plugin
@@ -8,32 +10,33 @@ import ckan.plugins.toolkit as toolkit
 
 from ckan.plugins import implements, interfaces
 from ckan.plugins import SingletonPlugin
+
 # import ckanext.fluent.plugin as fluent_plugin
 
 
-class TranslatePlugin(plugins.SingletonPlugin):
+class TranslatePlugin(plugins.SingletonPlugin, DefaultTranslation):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(interfaces.IConfigurable, inherit=True)
 
     # IConfigurer
 
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'translate')
+        toolkit.add_template_directory(config_, "templates")
+        toolkit.add_public_directory(config_, "public")
+        toolkit.add_resource("fanstatic", "translate")
 
-
-
-        config_['scheming.dataset_schemas'] = """
-        ckanext.translate:schemas/translate_dataset.json
-        """
-        config_['scheming.presets'] = """
+        # config_[
+        #     "scheming.dataset_schemas"
+        # ] = "ckanext.translate:schemas/translate_dataset.json"
+        config_[
+            "scheming.dataset_schemas"
+        ] = "ckanext.translate:schemas/ckan_dataset.yaml"
+        config_[
+            "scheming.presets"
+        ] = """
         ckanext.scheming:presets.json
         ckanext.fluent:presets.json
         """
-
-
 
     def configure(self, config):
         # Register your custom schema file(s)
